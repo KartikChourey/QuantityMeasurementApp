0dcb33c6e425171b99881cc6ca7d72e62cf54e73
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-03-17 15:19:30 +0530

    [Kartik Chourey] : added intial setup

---
+++ src/main/java/com/app/quantitymeasurement/dto/QuantityInputDTO.java
@@ -0,0 +1,17 @@
+package com.app.quantitymeasurement.dto;
+
+import lombok.Data;
+
+@Data
+public class QuantityInputDTO {
+	private QuantityDTO thisQuantityDTO;
+	private QuantityDTO thatQuantityDTO;
+	public QuantityDTO getThisQuantityDTO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public QuantityDTO getThatQuantityDTO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
